99ca6c6fd75ccd144b95bdec7496ce995b72a2b7
Bartosz Gaj <bartosz-gaj@wp.pl>
2017-05-21 22:55:23 +0200

    Files part 2

---
+++ Scripts/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("zetony", 0);
+        PlayerPrefs.SetInt("punkty", 0);
+	}
+	
+	
+}
